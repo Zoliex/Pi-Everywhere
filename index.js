adeff4e41f7948bb669f85aaa7064fe43a0b9bb1
Enzo Coquelle <enzo.coquelle06@gmail.com>
2023-07-21 09:43:30 +0200

    First commit !

---
+++ index.js
@@ -0,0 +1,13 @@
+//create a web server using express
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
